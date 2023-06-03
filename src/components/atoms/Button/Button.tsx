ec5e24831449599c1c5c3f6dd883faa770862a87
Mohit Raj Purohit <mohpuroh@publicisgroupe.net>
2023-06-04 00:15:27 +0530

    react comp

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,11 +2,13 @@ import React, { MouseEventHandler } from 'react';
 import { StyledButton } from './ButtonStyles';
 export interface ButtonProps {
 	label: string;
+	color?: string;
+	hoverColor?: string;
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 const Button = (props: ButtonProps) => {
-	return <StyledButton color="white" onClick={props.onClick}>{props.label}</StyledButton>;
+	return <StyledButton color={props.color} hoverColor={props.hoverColor} onClick={props.onClick}>{props.label}</StyledButton>;
 };
 
 export default Button;
---
+++ src/components/atoms/Button/ButtonStyles.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import isPropValid from '@emotion/is-prop-valid';
+
+interface StyledButtonProps {
+	color?: string;
+    hoverColor?: string;
+}
+
+const styles = css`
+	background-color: #4285f4;
+	color: white;
+	padding: 10px 20px;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+	font-size: 16px;
+	&:hover {
+		background-color: #357ae8;
+	}
+`;
+
+export const StyledButton = styled('button', {
+	shouldForwardProp: (prop) =>
+		isPropValid(prop) && prop !== 'color' && prop !== 'hoverColor',
+})<StyledButtonProps>(
+	(props) => css`
+		${styles};
+		color: ${props.color};
+		&:hover {
+			color: ${props.hoverColor};
+		}
+	`
+);
